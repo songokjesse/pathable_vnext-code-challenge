c964809a1b384cb4efc3f1a08093c94e7dce1d9d
Jesse Songok <jessesongok@gmail.com>
2020-01-13 08:55:51 +0300

    # Upgraded Meteor packages # Added bootstrap 4 cdn

--- ui/App.jsx
+++ ui/App.jsx
@@ -3,6 +3,21 @@ import { TEXTS } from '../shared/constants';
 
 export const App = () => (
   <div>
-    <h1>{TEXTS.HOME_TITLE}</h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        {TEXTS.HOME_TITLE}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+    </nav>
   </div>
 );
